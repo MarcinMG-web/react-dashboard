c2b813b988b5c37ccb9ebc86ad2c4b3e3a2c5501
MarcinMG-web <gierada.marcin@gmail.com>
2024-07-01 18:54:14 +0200

    feat:added standard version

--- src/ui/Footer.tsx
+++ src/ui/Footer.tsx
@@ -1,4 +1,5 @@
 import { Box, Typography } from '@mui/joy';
+import packageJson from '../../package.json';
 
 export default function Footer(): JSX.Element {
   const year = new Date().getFullYear();
@@ -6,7 +7,7 @@ export default function Footer(): JSX.Element {
   return (
     <Box component='footer' sx={{ py: 3 }}>
       <Typography level='body-xs' textAlign='center'>
-        © Dashboard App {year}
+        <h5>© Dashboard App {year}</h5> <h6> Wersja aplikacji: {packageJson.version}</h6>
       </Typography>
     </Box>
   );
