8ffd7005e8fc2d9369b37b53c40eecc1955341c1
MarcinMG-web <gierada.marcin@gmail.com>
2024-07-10 10:44:54 +0200

    style: udpated snacbar config

--- src/config/snackbarConfig.ts
+++ src/config/snackbarConfig.ts
@@ -16,8 +16,16 @@ export const StyledMaterialDesignContent = styled(MaterialDesignContent)(() => (
   '&.notistack-MuiContent-success': {
     backgroundColor:
       'var(--variant-solidBg, var(--joy-palette-success-solidBg, var(--joy-palette-success-500, #1F7A1F)))',
+    fontWeight: 600,
   },
   '&.notistack-MuiContent-error': {
     backgroundColor: 'var(--joy-palette-danger-700, #7D1212)',
+    fontWeight: 600,
+  },
+  '&.notistack-MuiContent-info': {
+    fontWeight: 600,
+  },
+  '&.notistack-MuiContent-warning': {
+    fontWeight: 600,
   },
 }));
